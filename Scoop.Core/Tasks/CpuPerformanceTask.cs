c061a86ef9052497557479d12ed1e21a49072334
okebai <okebai@gmail.com>
2015-01-18 17:36:22 +0100

    + Additional changes from previous commit

---
+++ Scoop.Core/Tasks/CpuPerformanceTask.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using Scoop.Core.Caching;
+using Scoop.Core.Tasks.Interfaces;
+
+namespace Scoop.Core.Tasks
+{
+    public class CpuPerformanceTask : Task
+    {
+        private static readonly Lazy<CpuPerformanceTask> _instance = new Lazy<CpuPerformanceTask>(() => new CpuPerformanceTask());
+        public static CpuPerformanceTask Instance { get { return _instance.Value; } }
+
+        public override string Name
+        {
+            get { return "CpuPerformanceTask"; }
+        }
+
+        private PerformanceCounter _cpuPerformanceCounter;
+        private PerformanceCounter CpuPerformanceCounter
+        {
+            get
+            {
+                return _cpuPerformanceCounter ?? (_cpuPerformanceCounter =
+                    new PerformanceCounter
+                    {
+                        CategoryName = "Processor",
+                        CounterName = "% Processor Time",
+                        InstanceName = "_Total"
+                    });
+            }
+        }
+
+        private CpuPerformanceTask() { }
+
+        public override ITask Execute(object state)
+        {
+            ReadCpu();
+
+            return this;
+        }
+
+        private void ReadCpu()
+        {
+            CacheHandler.Instance.SetLastUpdate<CpuPerformanceTask>(DateTime.Now);
+
+            var cpuValue = CpuPerformanceCounter.NextValue();
+
+            var taskResult = new PerformanceTaskResult(this, cpuValue);
+            CacheHandler.Instance.Set(taskResult);
+
+            TaskListener.HandleResult(taskResult);
+        }
+    }
+}
---
+++ Scoop.Core/Tasks/PerformanceTaskResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
+using Scoop.Core.Tasks.Interfaces;
+
+namespace Scoop.Core.Tasks
+{
+    public class PerformanceTaskResult : TaskResult
+    {
+        public PerformanceTaskResult(ITask task, params double[] values)
+            : base(task)
+        {
+            for (var i = 0; i < values.Length; i++)
+            {
+                Values.Add(i.ToString(), values[i]);
+            }
+        }
+
+        public override string CacheKey
+        {
+            get { return string.Format("{0}_{1}_{2}", TaskName, "PerformanceTaskResult", Timestamp.ToString("O")); }
+        }
+    }
+}
