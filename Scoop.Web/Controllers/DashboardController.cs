c2a21b4ab4004090a23a16ac26b953fb2b8000f9
okebai <okebai@gmail.com>
2015-02-07 14:10:28 +0100

    + Added Dashboard

---
+++ Scoop.Web/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scoop.Web.Models;
+using Scoop.Web.Models.PageModels;
+
+namespace Scoop.Web.Controllers
+{
+    [RoutePrefix("Dashboard")]
+    public class DashboardController : Controller
+    {
+        [Route("~/")]
+        [Route("~/Dashboard")]
+        public ActionResult Dashboard()
+        {
+            var model = new DashboardPageModel();
+
+            return View("Dashboard", model);
+        }
+    }
+}
---
+++ Scoop.Web/Models/PageModels/DashboardPageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using Newtonsoft.Json;
+using Scoop.Core.ConnectionStorage;
+
+namespace Scoop.Web.Models.PageModels
+{
+    public class DashboardPageModel : PageModel
+    {
+        public DashboardPageModel()
+        {
+            Title = "Dashboard";
+            
+        }
+    }
+}
