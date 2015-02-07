76e663241fb2024139ba947b3c36470a01561eca
okebai <okebai@gmail.com>
2015-02-07 14:11:11 +0100

    + Added ConnectionController + Added modal for handling connections

---
+++ Scoop.Web/Controllers/ConnectionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Scoop.Core.ConnectionStorage;
+using Scoop.Web.Models;
+
+namespace Scoop.Web.Controllers
+{
+    [RoutePrefix("Connection")]
+    public class ConnectionController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Connect")]
+        public ContentResult ConnectPost(ConnectionModel connection)
+        {
+            var connectionStorage = new CookieConnectionStorage(HttpContext);
+
+            connectionStorage.AddOrUpdateConnection(connection);
+
+            return Content(JsonConvert.SerializeObject(connectionStorage.Connections), "application/json");
+        }
+    }
+}
